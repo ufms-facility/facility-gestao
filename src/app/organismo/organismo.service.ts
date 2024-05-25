75fd4e9b0c8a4bbc7ffba78b2014ca79c55bc4e6
ufms-facility <168665498+ufms-facility@users.noreply.github.com>
2024-05-25 17:55:27 -0300

    List

--- src/app/organismo/organismo.service.ts
+++ src/app/organismo/organismo.service.ts
@@ -9,12 +9,16 @@ import { Organismo } from "./models/organismo.model";
 
 export class OrganismoService {
     private API = 'http://matheus@200.130.152.123:8080/api/v1/organismos';
-    
+
     constructor(private httpClient: HttpClient) {
-    
+
     }
 
     save(organismo: Organismo): Observable<Organismo> {
         return this.httpClient.post<Organismo>(this.API, organismo);
+    }
+
+    listarCursos(): Observable<any> {
+        return this.httpClient.get(this.API);
     }
 }
