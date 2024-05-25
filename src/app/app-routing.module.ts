a6eadc8c7a421e43efc6ed910ec0f99aedc536d2
ufms-facility <168665498+ufms-facility@users.noreply.github.com>
2024-05-25 16:20:18 -0300

    FooterHeaderHomeV1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './componentes/home/home.component';
+import { OrganismoCreateComponent } from './organismo/organismo-create/component/organismo-create.component';
 
 const routes: Routes = [{
   path: '',
@@ -10,7 +11,12 @@ const routes: Routes = [{
 {
   path: 'home',
   component: HomeComponent
-}];
+},
+{
+  path: 'novo-curso',
+  component: OrganismoCreateComponent
+}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
