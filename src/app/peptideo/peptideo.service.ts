2b0ae6cd06da8579f05ed1c20b466ae9fa44f0d0
ufms-facility <168665498+ufms-facility@users.noreply.github.com>
2024-06-06 16:07:22 -0300

    PepServiceFull

--- src/app/peptideo/peptideo.service.ts
+++ src/app/peptideo/peptideo.service.ts
@@ -1,9 +1,31 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Peptideo } from './models/peptideo.model';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PeptideoService {
+  private API = `http://${window.location.hostname}:8080/api/v1/peptideos`;
+  
+  constructor(private httpClient: HttpClient) {
 
-  constructor() { }
+  }
+
+  save(peptideo: Peptideo): Observable<Peptideo> {
+    return this.httpClient.post<Peptideo>(this.API, peptideo);
+  }
+  listarPeptideos(): Observable<any> {
+    return this.httpClient.get(this.API);
+  }
+  delete(id: number): Observable<any> {
+    return this.httpClient.delete(`${this.API}/${id}`);
+  }
+  update(id?: number, peptideo?: Peptideo): Observable<any> {
+    return this.httpClient.put(`${this.API}/${id}`, peptideo);
+  }
+  buscarPorId(id: number): Observable<any> {
+    return this.httpClient.get(`${this.API}/${id}`);
+  }
 }
